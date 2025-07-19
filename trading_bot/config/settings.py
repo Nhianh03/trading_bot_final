4986a860ef7546acb1bfde0f21b4bec172263c6c
Nhi Anh <167952731+Nhianh03@users.noreply.github.com>
2025-07-19 14:49:35 +0700

    Delete trading_bot directory

--- trading_bot/config/settings.py
+++
@@ -1,6 +0,0 @@
-# trading_bot/config/settings.py
-
-DEFAULT_SLIPPAGE_PCT = 0.003   #buy_limit_with_slippageính limit price dựa vào slippageGửi lệnh LIMIT có kiểm soát slippage (mặc định lấy từ config).
-DEFAULT_LEVERAGE = 5 # đòn bẩy x5
-DEFAULT_RETRIES = 3 # Số lần thử lệnh giao dịch trên Binance. khi gửi lệnh thất bại
-DEFAULT_SYMBOL = "BTCUSDT"
